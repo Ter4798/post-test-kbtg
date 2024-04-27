cfb15a8a50c134d56f32618309475e7bcf2daaa0
phatara.c <phatara.c@entronica.co.th>
2024-04-28 00:17:25 +0700

    add validateAllowanceTypes, validateAllowanceAmounts

--- tax/validateReq.go
+++ tax/validateReq.go
@@ -12,6 +12,12 @@ func ValidateRequest(req *Request) error {
 	if err := validateWHT(req); err != nil {
 		return err
 	}
+	if err := validateAllowanceTypes(req); err != nil {
+		return err
+	}
+	if err := validateAllowanceAmounts(req); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -25,6 +31,29 @@ func validateTotalIncome(req *Request) error {
 func validateWHT(req *Request) error {
 	if req.WHT < 0 || req.WHT > req.TotalIncome {
 		return errors.New("invalid WHT must be greater than zero and morn than TotalIncome")
+	}
+	return nil
+}
+
+func validateAllowanceTypes(req *Request) error {
+	validAllowanceTypes := map[string]bool{
+		"donation":  true,
+		"k-receipt": true,
+	}
+
+	for _, allowance := range req.Allowances {
+		if _, ok := validAllowanceTypes[allowance.AllowanceType]; !ok {
+			return errors.New("invalid allowance type type should be donation, k-receipt")
+		}
+	}
+	return nil
+}
+
+func validateAllowanceAmounts(req *Request) error {
+	for _, allowance := range req.Allowances {
+		if allowance.Amount < 0 {
+			return errors.New("invalid allowance amount must be greater than 0")
+		}
 	}
 	return nil
 }

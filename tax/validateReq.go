78933339832ec88662d0e94edfba94eb6c180b57
phatara.c <phatara.c@entronica.co.th>
2024-04-27 23:38:20 +0700

    refactor ValidateRequest , add validateWHT

--- tax/validateReq.go
+++ tax/validateReq.go
@@ -6,9 +6,25 @@ import (
 
 func ValidateRequest(req *Request) error {
 
+	if err := validateTotalIncome(req); err != nil {
+		return err
+	}
+	if err := validateWHT(req); err != nil {
+		return err
+	}
+	return nil
+}
+
+func validateTotalIncome(req *Request) error {
 	if req.TotalIncome <= 0 {
 		return errors.New("totalIncome must be greater than zero")
 	}
+	return nil
+}
 
+func validateWHT(req *Request) error {
+	if req.WHT < 0 || req.WHT > req.TotalIncome {
+		return errors.New("invalid WHT must be greater than zero and morn than TotalIncome")
+	}
 	return nil
 }

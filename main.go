6b2cc8bb6826d95d3cf0329b716083fd053f5d16
phatara.c <phatara.c@entronica.co.th>
2024-04-27 18:20:29 +0700

    add Graceful Shutdown

--- main.go
+++ main.go
@@ -1,14 +1,41 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
+	"context"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	e := echo.New()
+	port := fmt.Sprintf(":%s", os.Getenv("PORT"))
+	fmt.Printf("%s", port)
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, Go Bootcamp!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+
+	go func() {
+		if err := e.Start(port); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("ListenAndServe error: ", err)
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+
+	<-shutdown
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
+	fmt.Println("Shutting down the server")
 }

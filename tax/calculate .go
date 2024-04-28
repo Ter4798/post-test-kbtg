8e848d0b1c9ace8b7891ed0292ae1d7bf77fc928
phatara.c <phatara.c@entronica.co.th>
2024-04-28 10:51:02 +0700

    fix calculate case 1,000,001 - 2,000,000 and morn than 2,000,000 GraduatedTax and add calculateTaxLevels

--- tax/calculate .go
+++ tax/calculate .go
@@ -37,11 +37,10 @@ func calculateGraduatedTax(taxableIncome float64) float64 {
 	case taxableIncome <= 1000000:
 		tax = 35000 + 0.15*(taxableIncome-500000)
 	case taxableIncome <= 2000000:
-		tax = 135000 + 0.2*(taxableIncome-1000000)
+		tax = 110000 + 0.2*(taxableIncome-1000000)
 	default:
-		tax = 535000 + 0.35*(taxableIncome-2000000)
+		tax = 310000 + 0.35*(taxableIncome-2000000)
 	}
-
 	return tax
 }
 
@@ -57,13 +56,45 @@ func calculateNetTaxAndRefund(tax, wht float64) (float64, float64) {
 	return tax, taxRefund
 }
 
-func CalculateTax(totalIncome float64, wht float64, allowances []Allowance) (float64, float64) {
+func calculateTaxLevels(taxableIncome float64) []TaxLevel {
+	taxLevels := []TaxLevel{
+		{"0-150,000", 0.0},
+		{"150,001-500,000", 0.0},
+		{"500,001-1,000,000", 0.0},
+		{"1,000,001-2,000,000", 0.0},
+		{"2,000,001 ขึ้นไป", 0.0},
+	}
+
+	switch {
+	case taxableIncome <= 150000:
+		// No tax
+	case taxableIncome <= 500000:
+		taxLevels[1].Tax = 0.1 * (taxableIncome - 150000)
+	case taxableIncome <= 1000000:
+		taxLevels[1].Tax = 35000.0
+		taxLevels[2].Tax = 0.15 * (taxableIncome - 500000)
+	case taxableIncome <= 2000000:
+		taxLevels[1].Tax = 35000.0
+		taxLevels[2].Tax = 75000.0
+		taxLevels[3].Tax = 0.2 * (taxableIncome - 1000000)
+	default:
+		taxLevels[1].Tax = 35000.0
+		taxLevels[2].Tax = 75000.0
+		taxLevels[3].Tax = 200000.0
+		taxLevels[4].Tax = 0.35 * (taxableIncome - 2000000)
+	}
+
+	return taxLevels
+}
+
+func CalculateTax(totalIncome float64, wht float64, allowances []Allowance) (float64, float64, []TaxLevel) {
 	personalAllowance := 60000.0
 	maxDonation := 100000.0
 	maxKReceipt := 50000.0
 	totalDeduction := calculateDeductions(allowances, maxDonation, maxKReceipt) + personalAllowance
 	taxableIncome := calculateTaxableIncome(totalIncome, totalDeduction)
 	tax := calculateGraduatedTax(taxableIncome)
+	taxLevels := calculateTaxLevels(taxableIncome)
 	netTax, taxRefund := calculateNetTaxAndRefund(tax, wht)
-	return netTax, taxRefund
+	return netTax, taxRefund, taxLevels
 }

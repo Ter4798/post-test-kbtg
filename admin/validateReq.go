cc06da7409d03fea8b5db2dd66f0a5f69d9c2538
phatara.c <phatara.c@entronica.co.th>
2024-04-28 22:15:58 +0700

    add validateKReceiptAllowance

--- admin/validateReq.go
+++ admin/validateReq.go
@@ -4,17 +4,16 @@ import (
 	"errors"
 )
 
-func ValidateRequest(req *personalAllowanceRequest) error {
-
-	if err := validatePersonalAllowance(req); err != nil {
-		return err
+func validatePersonalAllowance(req *personalAllowanceRequest) error {
+	if req.Amount < 10000 || req.Amount > 100000 {
+		return errors.New("amount must be between 10000 and 100000")
 	}
 	return nil
 }
 
-func validatePersonalAllowance(req *personalAllowanceRequest) error {
-	if req.Amount < 10000 || req.Amount > 100000 {
-		return errors.New("amount must be between 10000 and 100000")
+func validateKReceiptAllowance(req *kReceiptAllowanceRequest) error {
+	if req.Amount < 0 || req.Amount > 100000 {
+		return errors.New("amount must be between 0 and 100000")
 	}
 	return nil
 }
